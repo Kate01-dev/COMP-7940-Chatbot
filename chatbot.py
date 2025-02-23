1a9db4aa012e7b3957617a90b14b48d9b66fc574
kate <wang_ke_ke@163.com>
2025-02-23 22:22:08 +0800

    lab4 finish

--- chatbot.py
+++ chatbot.py
@@ -3,13 +3,14 @@ from telegram.ext  import (Updater, CommandHandler, MessageHandler, Filters, Cal
 import configparser 
 import logging
 import redis
+from ChatGPT_HKBU import HKBU_ChatGPT
 
 global redis1
 
 def  main():
 #Load your token and create an Updater for your Bot
     config=configparser.ConfigParser()
-    config.read('7940_lab3/config.ini')
+    config.read('config.ini')
     updater=Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']),use_context=True) 
     dispatcher=updater.dispatcher
     global redis1
@@ -18,11 +19,6 @@ def  main():
                          port=(config['REDIS']['REDISPORT']),
                          decode_responses=(config['REDIS']['DECODE_RESPONSE']),
                          username=(config['REDIS']['USER_NAME']))
-    print(config['REDIS']['HOST'])
-    print(config['REDIS']['PASSWORD'])
-    print(config['REDIS']['REDISPORT'])
-    print(config['REDIS']['USER_NAME'])
-    print(config['REDIS']['DECODE_RESPONSE'])
     
     #You can set this logging module,so you will know when 
     # and why things do not work as expected Meanwhile, update your config.ini as:
@@ -30,8 +26,13 @@ def  main():
                         level=logging.INFO)
     
     #register a dispatcher to handle message: here we register an echo dispatcher
-    echo_handler=MessageHandler(Filters.text & (~Filters.command),echo)
-    dispatcher.add_handler(echo_handler)
+    # echo_handler=MessageHandler(Filters.text & (~Filters.command),echo)
+    # dispatcher.add_handler(echo_handler)
+
+    global chatgpt
+    chatgpt = HKBU_ChatGPT(config)
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command),equiped_chatgpt)
+    dispatcher.add_handler(chatgpt_handler)
 
     #on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
@@ -56,12 +57,19 @@ def add(update: Update, context: CallbackContext) -> None:
         global redis1
         logging.info(context.args[0])
         msg = context.args[0]
-        redis.int_or_str(msg)
+        print(msg)
+        redis1.incr(msg)
+        update.message.reply_text('You have said ' + msg + ' for ' +redis1.get(msg) + ' times.')
 
-        update.message.reply_text('You have said ' +msg+ ' for ' +
-                                  redis1.get(msg).decode('UTF_8') + ' times.')
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /add <keyword>')
+
+def equiped_chatgpt(update, context):
+    global chatgpt
+    reply_message = chatgpt.submit(update.message.text)
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
 
 if __name__=='__main__' :
     main()
--- read_config.py
+++ read_config.py
@@ -5,14 +5,31 @@ import configparser
 import redis
 
 config=configparser.ConfigParser()
-config.read('7940_lab3/config.ini')
-redis1 = redis.Redis(host=(config['REDIS']['HOST']),
-                         password=(config['REDIS']['PASSWORD']),
-                         port=(config['REDIS']['REDISPORT']),
-                         decode_responses=(config['REDIS']['DECODE_RESPONSE']),
-                         username=(config['REDIS']['USER_NAME']))
+config.read('config.ini')
+print(config['REDIS']['HOST'])
 print(config['REDIS']['PASSWORD'])
 print(config['REDIS']['REDISPORT'])
 print(config['REDIS']['USER_NAME'])
 print(config['REDIS']['DECODE_RESPONSE'])
+
+print(type(config['REDIS']['REDISPORT']))
+print(repr(config['REDIS']['HOST']))
+print("redis-17507.c322.us-east-1-2.ec2.redns.redis-cloud.com")
+
+# assert(config['REDIS']['HOST'] == "redis-17507.c322.us-east-1-2.ec2.redns.redis-cloud.com")
+# assert(config['REDIS']['PASSWORD'] == "BFIzvcGNrI1l3XPhHCs7DwIsXqcl0vR0")
+# assert(config['REDIS']['REDISPORT'] == 17507)
+# assert(config['REDIS']['USER_NAME'] == "default")
+# assert(config['REDIS']['DECODE_RESPONSE'] == True)
+
+redis1 = redis.Redis(host=(config['REDIS']['HOST']),
+                         password=(config['REDIS']['PASSWORD']),
+                         port=(config['REDIS']['REDISPORT']),
+                         decode_responses=(config['REDIS']['DECODE_RESPONSE']),
+                         username=("default"))
+success = redis1.set('foo', 'bar')
+# True
+result = redis1.get('foo')
+print(result)
+
 print(redis1.ping())
